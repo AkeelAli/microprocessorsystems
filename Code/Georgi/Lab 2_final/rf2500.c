5a02227dd230c56d3f0971ff8ea5bc0477c1a6f0
georgizk@gmail.com <georgizk@gmail.com@8c3b5eba-06f9-0298-2dd0-273c8ad6cfc7>
2011-11-24 19:13:51 +0000

    added all the config stuff... reading is still messed up, though it could be the writing screwing up. adding a break point seems to fix it.

--- Code/Georgi/Lab 2_final/rf2500.c
+++ Code/Georgi/Lab 2_final/rf2500.c
@@ -25,6 +25,7 @@ void init_spi (void) {
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7 | GPIO_Pin_6 | GPIO_Pin_5;
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
+
 	// need to define the MISO here somewhere
 
 
@@ -47,9 +48,8 @@ void init_spi (void) {
 	SysTick_Config (800);
 	rf_reset();
 
-
 	test_send();
-	test_send();
+	configure();
 //	SPI_I2S_SendData(SPI1, TI_CCxxx0_SRES);
 
 
@@ -134,19 +134,22 @@ u8 read_byte (u16 address) {
 	t2=1;
 	drop_css();
 	while(miso_high());
-	//wait ();
-
-//	SPI_I2S_SendData( SPI1,(address | TI_CCxxx0_READ_SINGLE)<<8);
+	wait ();
+	//while (SPI_I2S_GetFlagStatus(SPI1, SPI_I2S_FLAG_BSY) == RESET);
+	// keep reading untill 200 times the same value
+   	tmp = 200;
+  	while (tmp){
+		SPI_I2S_SendData( SPI1,(address | TI_CCxxx0_READ_SINGLE)<<8);
 		
-
-	while (SPI_I2S_GetFlagStatus(SPI1,SPI_I2S_FLAG_RXNE	) == RESET);
-
-	SPI_I2S_SendData( SPI1,(address | TI_CCxxx0_READ_SINGLE)<<8);
-	
-   	t = SPI_I2S_ReceiveData(SPI1);
-
+	   	t = SPI_I2S_ReceiveData(SPI1);
+		if (t2 == t)
+			tmp--;
+		else
+			tmp = 200;
+		t2 = t;
+	}
 	raise_css();
-
+//	wait();
 	return t;
 	
 
@@ -154,7 +157,7 @@ u8 read_byte (u16 address) {
 
 // this is weird. add wait
 void test_send(void) { 		 
-	write_byte (0x02,0x65);	  
+	write_byte (0x02,0x04);	  
 	
 	t3 = read_byte(0x02);   
 
@@ -182,5 +185,61 @@ void SysTick_Handler(void) {
 		_timer_lock = false; 
 		_called_once = false;
 	}
+
+}
+
+u8 set_config (u16 address, u16 byte) {
+ 	 write_byte (address,byte);	  
+	 wait();
+	 if (byte != read_byte(address))
+	 	return 1;
+	 else
+		return 0;
+}
+
+u8 configure (void) {
+		
+	   while (set_config(TI_CCxxx0_IOCFG0,		SETTING_IOCFG0				)) ;
+	   while (set_config(TI_CCxxx0_MCSM1,		SETTING_MCSM1				)) ;
+	   while (set_config(TI_CCxxx0_MCSM0,		SETTING_MCSM0				)) ;
+	   while (set_config(TI_CCxxx0_PKTLEN,		SETTING_PKTLEN				)) ;
+	   while (set_config(TI_CCxxx0_PKTCTRL0,	SETTING_PKTCTRL0			)) ;	 
+ 	   while (set_config(TI_CCxxx0_FSCTRL1,		SMARTRF_SETTING_FSCTRL1		)) ;
+	   while (set_config(TI_CCxxx0_IOCFG0,		SMARTRF_SETTING_FSCTRL0		)) ;
+	   while (set_config(TI_CCxxx0_FREQ2,		SMARTRF_SETTING_FREQ2		)) ;
+	   while (set_config(TI_CCxxx0_FREQ1,		SMARTRF_SETTING_FREQ1		)) ;
+	   while (set_config(TI_CCxxx0_FREQ0,		SMARTRF_SETTING_FREQ0		)) ;
+	   while (set_config(TI_CCxxx0_MDMCFG4,		SMARTRF_SETTING_MDMCFG4		)) ;
+	   while (set_config(TI_CCxxx0_MDMCFG3,		SMARTRF_SETTING_MDMCFG3		)) ;
+	   while (set_config(TI_CCxxx0_MDMCFG2,		SMARTRF_SETTING_MDMCFG2		)) ;
+	   while (set_config(TI_CCxxx0_MDMCFG1,		SMARTRF_SETTING_MDMCFG1		)) ;
+	   while (set_config(TI_CCxxx0_MDMCFG0,		SMARTRF_SETTING_MDMCFG0		)) ;
+	   while (set_config(TI_CCxxx0_CHANNR,		SMARTRF_SETTING_CHANNR		)) ;
+	   while (set_config(TI_CCxxx0_DEVIATN,		SMARTRF_SETTING_DEVIATN		)) ;
+	   while (set_config(TI_CCxxx0_FREND1,		SMARTRF_SETTING_FREND1		)) ;
+	   while (set_config(TI_CCxxx0_FREND0,		SMARTRF_SETTING_FREND0		)) ;
+	   while (set_config(TI_CCxxx0_MCSM0,		SMARTRF_SETTING_MCSM0		)) ;
+	   while (set_config(TI_CCxxx0_FOCCFG,		SMARTRF_SETTING_FOCCFG		)) ;
+	   while (set_config(TI_CCxxx0_BSCFG,		SMARTRF_SETTING_BSCFG		)) ;
+	   while (set_config(TI_CCxxx0_AGCCTRL2,	SMARTRF_SETTING_AGCCTRL2	)) ;
+	   while (set_config(TI_CCxxx0_AGCCTRL1,	SMARTRF_SETTING_AGCCTRL1	)) ;
+	   while (set_config(TI_CCxxx0_AGCCTRL0,	SMARTRF_SETTING_AGCCTRL0	)) ;
+	   while (set_config(TI_CCxxx0_FSCAL3,		SMARTRF_SETTING_FSCAL3		)) ;
+	   while (set_config(TI_CCxxx0_FSCAL2,		SMARTRF_SETTING_FSCAL2		)) ;
+	   while (set_config(TI_CCxxx0_FSCAL1,		SMARTRF_SETTING_FSCAL1		)) ;
+	   while (set_config(TI_CCxxx0_FSCAL0,		SMARTRF_SETTING_FSCAL0		)) ;
+	   while (set_config(TI_CCxxx0_FSTEST,		SMARTRF_SETTING_FSTEST		)) ;
+	   while (set_config(TI_CCxxx0_TEST2,		SMARTRF_SETTING_TEST2		)) ;
+	   while (set_config(TI_CCxxx0_TEST1,		SMARTRF_SETTING_TEST1		)) ;
+	   while (set_config(TI_CCxxx0_TEST0,		SMARTRF_SETTING_TEST0		)) ;
+	   while (set_config(TI_CCxxx0_FIFOTHR,		SMARTRF_SETTING_FIFOTHR		)) ;
+	   while (set_config(TI_CCxxx0_IOCFG2,		SMARTRF_SETTING_IOCFG2		)) ;
+	   while (set_config(TI_CCxxx0_PKTCTRL1,	SMARTRF_SETTING_PKTCTRL1	)) ;
+	   while (set_config(TI_CCxxx0_PKTCTRL0,	SMARTRF_SETTING_PKTCTRL0	)) ;
+	   while (set_config(TI_CCxxx0_ADDR,		SMARTRF_SETTING_ADDR		)) ;
+	   while (set_config(TI_CCxxx0_PKTLEN,		SMARTRF_SETTING_PKTLEN		)) ;
+
+	   t3 = read_byte(TI_CCxxx0_IOCFG0);
+	   return 0;
 
 }
