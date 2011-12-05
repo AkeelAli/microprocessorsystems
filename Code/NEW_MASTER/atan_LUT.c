63fb33eb90d468320da5d56ec443052d1dcb0147
agilowen@gmail.com <agilowen@gmail.com@8c3b5eba-06f9-0298-2dd0-273c8ad6cfc7>
2011-12-05 06:55:20 +0000

    removed extraneous gesture stuff + updated project file settings.

--- Code/NEW_MASTER/atan_LUT.c
+++ Code/NEW_MASTER/atan_LUT.c
@@ -1,57 +1,67 @@
 
 #include "stm32f10x_conf.h"
 #include "atan_LUT.h"
+#include <math.h>
 
-/* Angles go up to 45 degrees since atan(x) = 90 - atan(1/x) x e (0,45) */
-/* Input is a number between 0 and 1 scaled by 100 (0 ... 100)*/
-static const float atan_lookup[101] =
-{0,1,1,2,3,3,4,4,5,5,6,7,7,8,8,9,9,10,10,11,12,12,13,
-13,14,14,15,15,16,16,17,17,18,19,19,20,20,21,21,22,22,
-23,23,24,24,24,25,25,26,26,27,27,28,28,29,29,29,30,30,
-31,31,32,32,32,33,33,34,34,34,35,35,36,36,36,37,37,37,
-38,38,38,39,39,40,40,40,41,41,41,42,42,42,42,43,43,43,
-44,44,44,45,45,45};
+#define TO_DEG_FACTOR 57.29577951 
+#define LUT_SIZE 100
 
-/*
- * Arctan Lookup table Approximation
- * Computed from a minimal table of 101 entries
- * The remaining range is done via the modification:                 `
- * Arctan(x) = 90 - Arctan(1/x)	for X in [1, ...] and a result in [45, 90]
- * Calculation based on degrees and result in degrees
- * Missing : handle overflow big values
- * @param: X
- * @return: arctan(X)
- */
-float atan_table(float x) {
-	u16 temp;
-	float val;
-	if (x < 0) {							// if negative
-		val = -(x*100);						// scale by 100, take absolute value
-		if(val > 100) { 				 	// if x > 1.00
-			if (val > 10000) {			 	// if too big prevent overflow
-				val = 100;				 	// set a barrier
-			}
-			temp = (u16) (val);		 		// cast into u16 type
-			temp = 10000/temp;			 	// 1/x
-			val = atan_lookup[temp]-90;	 	// -(90 - arctan(1/x))
-		} else {
-			temp = (u16) (val);		 		// cast into u16 type
-			val = -atan_lookup[temp];
-		}
-	} else {								// if positive
-		val = x*100;						//scale by 100
-		if(val > 100) { 				 	// if x > 1.00
-			if (val > 10000) {			 	// if too big prevent overflow
-				val = 100;				 	// set a barrier
-			}
-			temp = (u16) (val);				// cast into u16 type
-			temp = 10000/temp;			 	// 1/x
-			val = 90-atan_lookup[temp];  	// 90 - arctan(1/x)
-		} else {
-			temp = (u16) (val);				// cast into u16 type
-			val = atan_lookup[temp];
-		}
+void init_lut(double lut_1[],double lut_2[]){
+	int i;
+	double value;	
+
+	for(i=0;i<LUT_SIZE;i++){
+		value=(i+1)/100.;
+		lut_1[i]=atan(value)*TO_DEG_FACTOR;				
 	}
-	return val;
+	
+	for(i=0;i<LUT_SIZE;i++){
+		value=(i+1)/10. + 1;
+		lut_2[i]=atan(value)*TO_DEG_FACTOR;				
+	}	
+}
+
+float atan_table(float value){
+	static int initialized=0;
+	//2 lookup tables 
+	static double lut_1[LUT_SIZE];
+	static double lut_2[LUT_SIZE];
+
+	int sign=1;
+	float ans=0.0;
+	int index=0;
+
+	if (!initialized){
+		init_lut(lut_1,lut_2);
+		initialized=1;
+	}
+
+	if (value < 0){
+		sign=-1;
+		value*=-1;
+	}
+
+	if(value <= 1){
+		index=(int) (value*100 - 1);
+		ans=lut_1[index];
+	}
+	else if(value <= 11){
+		index=(int) ((value - 1)*10 - 1);
+		ans=lut_2[index];
+	}
+	else if (value <= 11.430)
+	    ans=85.0;
+	else if (value <= 14.301)
+	    ans=86.0;
+	else if (value <= 19.081)
+	    ans=87.0;
+	else if (value <= 28.636)
+	    ans=88.0;
+	else if (value <= 57.290)
+	    ans=89.0;
+	else
+	    ans=90.0;
+
+	return ans*sign;
 }
 
--- Code/NEW_MASTER/main.c
+++ Code/NEW_MASTER/main.c
@@ -9,14 +9,13 @@
 
 
 int main(void){
-	
+				  extern u8 latching;
 	initAccGyro();
 	initTIM();
 	init_timer();
 	init_protocol();
 	init_spi();
-
-
+	
 	go_wait();
  
 
